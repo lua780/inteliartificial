76a6461357375e679d892061a216d7b00e9b2c3f
nathaly-pb <128197388+lua780@users.noreply.github.com>
2024-08-16 22:14:12 -0300

    S

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
+{
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Aternativa 2"
+    ]
+}
 
 const perguntas = [
     {
